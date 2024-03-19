5219b2306b223b5fc33f51d524e3f8343b941902
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-03-19 15:15:03 +0300

    Container methods testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import IntContainer.IntContainer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IntContainer container = new IntContainer();
+
+        // adding elements
+        container.addElement(5);
+        container.addElement(100);
+        container.addElement(2);
+
+        container.printElements();
+        System.out.println("Container size: " + container.getSize());
+
+        System.out.println("--------------------------");
+
+        // get element
+        System.out.println(container.getElementFromIndex(2));
+        System.out.println(container.getElementFromIndex(0));
+
+        System.out.println("--------------------------");
+
+        // element removal
+        container.removeElementFromIndex(0);
+        container.removeElementFromIndex(1);
+
+        container.printElements();
+        System.out.println("Container size: " + container.getSize());
     }
 }
