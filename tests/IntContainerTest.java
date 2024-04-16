e4aaacb5dfae9fa20e41bce2607fceecbb14fd75
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 16:41:09 +0300

    added removeElement test

--- tests/IntContainerTest.java
+++ tests/IntContainerTest.java
@@ -1,11 +1,19 @@
 import IntContainer.IntContainer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IntContainerTest {
+
+    private IntContainer container;
+
+    @BeforeEach
+    public void setUp() {
+        container = new IntContainer();
+    }
+
     @Test
     public void testAddElementAndGetElement() {
-        IntContainer container = new IntContainer();
         container.addElement(5);
         container.addElement(10);
         container.addElement(15);
@@ -13,5 +21,18 @@ public class IntContainerTest {
         assertEquals(5, container.getElementFromIndex(0));
         assertEquals(10, container.getElementFromIndex(1));
         assertEquals(15, container.getElementFromIndex(2));
+    }
+
+    @Test
+    public void testRemoveElement() {
+        container.addElement(5);
+        container.addElement(10);
+        container.addElement(15);
+
+        container.removeElementFromIndex(1);
+
+        assertEquals(2, container.getSize());
+        assertEquals(5, container.getElementFromIndex(0));
+        assertEquals(15, container.getElementFromIndex(1));
     }
 }
