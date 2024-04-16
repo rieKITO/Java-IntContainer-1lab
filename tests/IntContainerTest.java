3e86ceb1777c3bca179fdbd86360935aefba04d7
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 16:33:51 +0300

    added testAddElementAndGetElement test

--- tests/IntContainerTest.java
+++ tests/IntContainerTest.java
@@ -1,2 +1,17 @@
+import IntContainer.IntContainer;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntContainerTest {
+    @Test
+    public void testAddElementAndGetElement() {
+        IntContainer container = new IntContainer();
+        container.addElement(5);
+        container.addElement(10);
+        container.addElement(15);
+
+        assertEquals(5, container.getElementFromIndex(0));
+        assertEquals(10, container.getElementFromIndex(1));
+        assertEquals(15, container.getElementFromIndex(2));
+    }
 }
